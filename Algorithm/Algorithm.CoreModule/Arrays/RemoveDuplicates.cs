597bb8cea9ede42c7755f703307520200f921e76
ivanfeng <ivanfeng0212@gmail.com>
2024-11-12 23:11:03 +0800

    新增 RemoveDuplicates 練習

---
+++ Algorithm/Algorithm.CoreModule/Arrays/RemoveDuplicates.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.CoreModule.Arrays
+{
+    /*
+    nums 是已排序的整數陣列。
+    只修改陣列並在常數空間內完成。
+    輸入陣列可能是空的。
+     */
+
+    public class RemoveDuplicates
+    {
+        public int FindMaxUniqueLength(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0) return 0;
+
+            int slowIndex = 0;
+            for (int fastIndex = 1; fastIndex < numbers.Length; fastIndex += 1)
+            {
+                // 數值重複，fastIndex 繼續往前，slowIndex 不動
+                if (numbers[slowIndex] == numbers[fastIndex])
+                    continue;
+
+                // 當發現不重複的元素時，
+                // slowIndex + 1，
+                // 替換元素為新找到的
+                slowIndex += 1;
+                numbers[slowIndex] = numbers[fastIndex];
+            }
+
+            // 返回 Index + 1 即為最大長度
+            return slowIndex + 1;
+        }
+    }
+}
--- Algorithm/Algorithm.Test/ArrayTest.cs
+++ Algorithm/Algorithm.Test/ArrayTest.cs
@@ -11,7 +11,7 @@ namespace Algorithm.Test
     public class ArrayTest
     {
         [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 9, new int[] { 2, 7 })]
-        [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 8, new int[] { 2, 7 })]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 8, new int[] { 1, 7 })]
         public void GivenSortedNumbers_WhenFindTwoSumIndices_ThenReturnCorrectIndexPositions(int[] numbers, int target, int[] expects)
         {
             var twoSum = new TwoSum();
@@ -19,6 +19,17 @@ namespace Algorithm.Test
             var results = twoSum.FindTwoSumIndices(numbers, target);
 
             Assert.That(results, Is.EqualTo(expects));
+        }
+
+        [TestCase(new int[] { 0, 0, 1, 2, 2, 4, 4 }, 4)]
+        [TestCase(new int[] { 0, 0, 1, 2, 2, 3, 4, 4 }, 5)]
+        public void GivenSortedNumbers_WhenFindMaxUniqueLength_ThenReturnMaxUniqueLength(int[] numbers, int expected)
+        {
+            var removeDuplicates = new RemoveDuplicates();
+
+            var results = removeDuplicates.FindMaxUniqueLength(numbers);
+
+            Assert.That(results, Is.EqualTo(expected));
         }
     }
 }
