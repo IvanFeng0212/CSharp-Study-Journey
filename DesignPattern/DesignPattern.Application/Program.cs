0461b6e853522773ac08d6aafaa3571c761db4f3
ivanfeng <ivanfeng0212@gmail.com>
2024-11-16 10:55:23 +0800

    新增裝飾者模式練習

--- DesignPattern/DesignPattern.Application/Program.cs
+++ DesignPattern/DesignPattern.Application/Program.cs
@@ -1,7 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DesignPattern.CoreModule.Interfaces.Decorator;
+using DesignPattern.CoreModule.Models.Decorator;
 using DesignPattern.CoreModule.Models.Observer;
 
-ObserverPattern();
+//// 觀察者模式
+// ObserverPattern();
+
+//// 裝飾者模式
+// DecoratorPattern();
 
 #region 觀察者模式
 
@@ -11,6 +17,9 @@ ObserverPattern();
 
 使用場景
 當一個物件的狀態變化需要同時更新其他物件時。
+
+應用實例
+拍賣系統：拍賣官作為主題，競價者作為觀察者，拍賣價格更新時通知所有競價者
 
 優點
 抽象耦合：觀察者和主題之間是抽象耦合的。
@@ -59,3 +68,60 @@ static void ObserverPattern()
 }
 
 #endregion 觀察者模式
+
+#region 裝飾者模式
+
+/*
+意圖
+動態地為一個物件添加額外的職責，同時不改變其結構。
+裝飾器模式提供了一種靈活的替代繼承方式來擴展功能。
+
+主要解決的問題
+避免透過繼承引入靜態特徵，特別是在子類別數量急劇膨脹的情況下。
+允許在運行時動態地新增或修改物件的功能。
+
+使用場景
+當需要在不增加大量子類別的情況下擴展類別的功能。
+當需要動態地新增或撤銷物件的功能。
+
+應用實例
+孫悟空的72變：孫悟空透過變化獲得新的能力。
+畫框裝飾畫：一幅畫可以透過添加玻璃和畫框來增強其展示效果。
+
+優點
+低耦合：裝飾類別和被裝飾類別可以獨立變化，互不影響。
+靈活性：可以動態地新增或撤銷功能。
+替代繼承：提供了一種繼承之外的擴展物件功能的方式。
+
+缺點
+複雜性：多層裝飾可能導致系統複雜性增加。
+
+使用建議
+在需要動態擴充功能時，請考慮使用裝飾器模式。
+保持裝飾者和特定組件的介面一致，以確保靈活性。
+
+注意事項
+裝飾器模式可以替代繼承，但應謹慎使用，避免過度裝飾導致系統複雜。
+*/
+static void DecoratorPattern()
+{
+    IBeverage blackTea = new BlackTea();
+    Console.WriteLine($"Beverage is:{blackTea.GetDescription()}，Price is:{blackTea.Cost()}");
+
+    IBeverage blackTeaWithMilk = new MilkDecorator(blackTea);
+    Console.WriteLine($"Beverage is:{blackTeaWithMilk.GetDescription()}，Price is:{blackTeaWithMilk.Cost()}");
+
+    IBeverage blackTeaWithMilkAndBubble = new BubbleDecorator(blackTeaWithMilk);
+    Console.WriteLine($"Beverage is:{blackTeaWithMilkAndBubble.GetDescription()}，Price is:{blackTeaWithMilkAndBubble.Cost()}");
+
+    IBeverage greenTea = new GreenTea();
+    Console.WriteLine($"Beverage is:{greenTea.GetDescription()}，Price is:{greenTea.Cost()}");
+
+    IBeverage greenTeaWithBubble = new BubbleDecorator(greenTea);
+    Console.WriteLine($"Beverage is:{greenTeaWithBubble.GetDescription()}，Price is:{greenTeaWithBubble.Cost()}");
+
+    IBeverage greenTeaWithMilk = new MilkDecorator(greenTea);
+    Console.WriteLine($"Beverage is:{greenTeaWithMilk.GetDescription()}，Price is:{greenTeaWithMilk.Cost()}");
+}
+
+#endregion 裝飾者模式
