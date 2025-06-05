9518df09cab353f299d529e232d01703d8ebae1d
ivanfeng <ivanfeng0212@gmail.com>
2025-06-05 22:14:04 +0800

    新增 977 解法

---
+++ LeetCode.CoreModule/Array/SortedSquares_977.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CoreModule.Array
+{
+    public class SortedSquares_977
+    {
+        public int[] SortedSquares(int[] nums)
+        {
+            int[] results = new int[nums.Length];
+            int resultLastIndex = results.Length - 1;
+            int left = 0;
+            int right = nums.Length - 1;
+            int tempLeft = int.MinValue;
+            int tempRight = int.MinValue;
+            while (left <= right)
+            {
+                tempLeft = nums[left] * nums[left];
+                tempRight = nums[right] * nums[right];
+                if (tempLeft >= tempRight)
+                {
+                    results[resultLastIndex] = tempLeft;
+                    left += 1;
+                }
+                else
+                {
+                    results[resultLastIndex] = tempRight;
+                    right -= 1;
+                }
+
+                resultLastIndex -= 1;
+            }
+
+            return results;
+        }
+    }
+}
