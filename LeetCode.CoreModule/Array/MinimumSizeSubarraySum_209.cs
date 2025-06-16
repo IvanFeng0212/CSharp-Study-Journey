af2c5eff6847d2a9e7ce901e2523e3cb1a99bbd0
ivanfeng <ivanfeng0212@gmail.com>
2025-06-16 22:17:52 +0800

    新增 209 練習

---
+++ LeetCode.CoreModule/Array/MinimumSizeSubarraySum_209.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.CoreModule.Array
+{
+    /*
+    Given an array of positive integers nums and a positive integer target,
+    return the minimal length of a subarray whose sum is greater than or equal to target.
+    If there is no such subarray, return 0 instead.
+
+    Constraints:
+    1 <= target <= 10^9
+    1 <= nums.length <= 10^5
+    1 <= nums[i] <= 10^4
+
+    Example 1:
+    Input: target = 7, nums = [2,3,1,2,4,3]
+    Output: 2
+    Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+
+    Example 2:
+    Input: target = 4, nums = [1,4,4]
+    Output: 1
+
+    Example 3:
+    Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+    Output: 0
+    */
+
+    public class MinimumSizeSubarraySum_209
+    {
+        public int MinSubArrayLen(int target, int[] nums)
+        {
+            int tempSum = 0;
+            int minSubArrayLen = int.MaxValue;
+            int slowIndex = 0;
+            for (int quickIndex = 0; quickIndex < nums.Length; quickIndex += 1)
+            {
+                tempSum += nums[quickIndex];
+                while (tempSum >= target)
+                {
+                    minSubArrayLen = Math.Min(minSubArrayLen, (quickIndex - slowIndex) + 1);
+
+                    tempSum -= nums[slowIndex];
+                    slowIndex += 1;
+                }
+            }
+
+            return minSubArrayLen == int.MaxValue ? 0 : minSubArrayLen;
+        }
+    }
+}
